3d4eab85059fd8d824d04ad3b8da02255344a76a
Štěpán Kakeš <91247524+StepanKakes@users.noreply.github.com>
2022-04-07 18:59:11 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,59 +1,49 @@
 stred=[2,2]
 A=[2,1]
 B=[0,2]
-C=[4,2]
-a1=A[1]
-b1=B[1]
-c1=C[1]
-x=1
-y=0
+C=[3,2]
+x=A
+y=1
 counter=0
 def on_forever():
-    led.plot(stred[0], stred[1])
+    led.plot_brightness(stred[0], stred[1], 100)
     led.plot(A[0],A[1])
     led.plot(B[0],B[1])
     led.plot(C[0],C[1])
+    
 basic.forever(on_forever)
 
 def on_button_pressed_a():
-    global A, B, C, counter, a1, b1, c1
+    global A, B, C, counter, x,y
     basic.clear_screen()
-    counter+=1
-    if counter ==1:
-        A.reverse()
-    elif counter ==2:
-        A[0]+=a1
-        A[1]+=a1
-    elif counter ==3:
-        A.reverse()
-    elif counter ==4:
-        A[0]-=a1
-        A[1]-=a1
-        counter=0
+    while counter!=3:
+        counter+=1
+        console.log_value("counter", counter)
+        console.log_value("A", A)
+        console.log_value("b", B)
+        console.log_value("C", C)
+        console.log_value("x", x)
+        x.reverse()
+        if x[1]==0:
+            x[1]=4
+        elif x[1]==4:
+            x[1]=0
+        elif x[1]==3:
+            x[1]=1
+        elif x[1]==1:
+            x[1]=3
+        if counter==1:
+            x=B
+        if counter==2:
+            x=C
+        
+    counter=0
+    x=A
 
-    if counter ==1:
-        B.reverse()
-    elif counter ==2:
-        B[0]+=b1
-        B[1]+=b1
-    elif counter ==3:
-        B.reverse()
-    elif counter ==4:
-        B[0]-=b1
-        B[1]-=b1
-        counter=0
+    
+   
 
-    if counter ==1:
-        C.reverse()
-    elif counter ==2:
-        C[0]+=c1
-        C[1]+=c1
-    elif counter ==3:
-        C.reverse()
-    elif counter ==4:
-        C[0]-=c1
-        C[1]-=c1
-        counter=0
+    
     
     #B.reverse()
     #C.reverse()
--- main.ts
+++ main.ts
@@ -1,15 +1,12 @@
 let stred = [2, 2]
 let A = [2, 1]
 let B = [0, 2]
-let C = [4, 2]
-let a1 = A[1]
-let b1 = B[1]
-let c1 = C[1]
-let x = 1
-let y = 0
+let C = [3, 2]
+let x = A
+let y = 1
 let counter = 0
 basic.forever(function on_forever() {
-    led.plot(stred[0], stred[1])
+    led.plotBrightness(stred[0], stred[1], 100)
     led.plot(A[0], A[1])
     led.plot(B[0], B[1])
     led.plot(C[0], C[1])
@@ -19,44 +16,33 @@ basic.forever(function on_forever() {
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
     basic.clearScreen()
-    counter += 1
-    if (counter == 1) {
-        A.reverse()
-    } else if (counter == 2) {
-        A[0] += a1
-        A[1] += a1
-    } else if (counter == 3) {
-        A.reverse()
-    } else if (counter == 4) {
-        A[0] -= a1
-        A[1] -= a1
-        counter = 0
+    while (counter != 3) {
+        counter += 1
+        console.logValue("counter", counter)
+        console.logValue("A", A)
+        console.logValue("b", B)
+        console.logValue("C", C)
+        console.logValue("x", x)
+        x.reverse()
+        if (x[1] == 0) {
+            x[1] = 4
+        } else if (x[1] == 4) {
+            x[1] = 0
+        } else if (x[1] == 3) {
+            x[1] = 1
+        } else if (x[1] == 1) {
+            x[1] = 3
+        }
+        
+        if (counter == 1) {
+            x = B
+        }
+        
+        if (counter == 2) {
+            x = C
+        }
+        
     }
-    
-    if (counter == 1) {
-        B.reverse()
-    } else if (counter == 2) {
-        B[0] += b1
-        B[1] += b1
-    } else if (counter == 3) {
-        B.reverse()
-    } else if (counter == 4) {
-        B[0] -= b1
-        B[1] -= b1
-        counter = 0
-    }
-    
-    if (counter == 1) {
-        C.reverse()
-    } else if (counter == 2) {
-        C[0] += c1
-        C[1] += c1
-    } else if (counter == 3) {
-        C.reverse()
-    } else if (counter == 4) {
-        C[0] -= c1
-        C[1] -= c1
-        counter = 0
-    }
-    
+    counter = 0
+    x = A
 })
